2a8ddccafb1f42c6982060f7a3fb47bfb9fdd8a0
GalKubani <75381160+GalKubani@users.noreply.github.com>
2021-01-20 16:49:55 +0200

    Delete db directory

--- db/mongoose.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose.connect(`mongodb+srv://galkubani:b8RQ7KkbeVmaE8lI@firstcluster.syjos.mongodb.net/checkers-api?retryWrites=true&w=majority`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-});
